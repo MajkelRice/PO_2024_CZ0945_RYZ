06ee3acbe523d003d3a12daa367794a05b8a7684
Michal Ryz <michalryz2003@gmail.com>
2024-10-11 16:15:28 +0200

    Lab 1

---
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,29 @@
+package agh.ics.oop;
+import agh.ics.oop.model.MoveDirection;
+
+
+public class OptionsParser {
+    public static MoveDirection[] parse(String args[]){
+        int len = args.length;
+        var directions = new MoveDirection[len];
+
+        for (int i = 0; i < len; i++) {
+            switch (args[i]) {
+                case "f":
+                    directions[i] = MoveDirection.FORWARD;
+                    break;
+                case "l":
+                     directions[i] = MoveDirection.LEFT;
+                     break;
+                case "r":
+                     directions[i] = MoveDirection.RIGHT;
+                     break;
+                case "b":
+                    directions[i] = MoveDirection.BACKWARD;
+                    break;
+
+            }
+        }
+    return directions;
+    }
+}
